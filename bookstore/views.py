7db5def873750d2f76b59e54f1909e40a5addec5
Chiemeka Alim <chiemeka.alim@andela.co>
2016-11-23 17:50:20 +0100

    Add base and home template

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class HomePageView(View):
+    def get(self, request):
+        """
+
+        :param request:
+        :return:
+        """
+
+        return render(request, 'bookstore/home.html', {})
